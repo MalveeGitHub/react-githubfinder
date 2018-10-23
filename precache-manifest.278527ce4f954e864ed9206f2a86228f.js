9181b1b1e9ea7f90aa1874b628bc21648f7b0418
alvee <you@example.com>
2018-10-23 03:09:09 -0400

    Updates

---
+++ precache-manifest.278527ce4f954e864ed9206f2a86228f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9fec620b983ffd2dc16",
+    "url": "/react-githubfinder/static/js/runtime~main.f9fec620.js"
+  },
+  {
+    "revision": "21df7b736e52c95f1454",
+    "url": "/react-githubfinder/static/js/main.21df7b73.chunk.js"
+  },
+  {
+    "revision": "c536951d5d433e6bbcec",
+    "url": "/react-githubfinder/static/js/1.c536951d.chunk.js"
+  },
+  {
+    "revision": "21df7b736e52c95f1454",
+    "url": "/react-githubfinder/static/css/main.bb6030e2.chunk.css"
+  },
+  {
+    "revision": "5455fdc013b6e1d177928f9a24040aff",
+    "url": "/react-githubfinder/index.html"
+  }
+];
